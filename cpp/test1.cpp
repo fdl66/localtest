6eccbef83ce320dea59a698d1a729e22a787a5f1
fdl66 <fan0816fan@163.com>
2016-06-10 21:48:42 +0800

    debug""

--- cpp/test1.cpp
+++ cpp/test1.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
 	cout<<"hello world!"<<endl;cout<<"good moring!"<<endl;
-	for("int i=0;i<100;i++"){
+	for(int i=0;i<100;i++){
     	cout<<i<<endl;
 	}
 	return 0;
